9c3a85a67ca411a7aec664debec5436c325c7a9f
Bruno <bruno.g.p897@gmail.com>
2021-08-24 16:57:49 -0300

    Consumindo API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'api'
+    
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 15,
+          name: 'cartão',
+          amount: 548,
+          type: 'deposit',
+          category: 'gastos',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
